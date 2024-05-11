e0ded964c489feb248b119bca81c99dd37badb84
Dinh Viet Luong <dinhvietluong99@gmail.com>
2024-05-10 21:14:38 -0400

    Numbering System

--- src/main/java/org/cockyadolescents/truechessthegame/Controller.java
+++ src/main/java/org/cockyadolescents/truechessthegame/Controller.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -34,6 +35,11 @@ public class Controller {
     private static Vector<Pair<Integer, Integer>> possibleMoves;
     private static String playingSide = "White";
 
+    @FXML
+    private GridPane buttonBoard, labelBoard;
+    private VBox leftNumbers;
+    private HBox topNumbers;
+
     public void startGame() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("maingame.fxml"));
         Stage window = (Stage) welcomeText.getScene().getWindow();
@@ -42,7 +48,13 @@ public class Controller {
         window.setScene(scene);
         window.show();
 
-        createBoard((GridPane) root.lookup("#board"), (GridPane) root.lookup("#boardBackground"));
+        buttonBoard = (GridPane) root.lookup("#buttonBoard");
+        labelBoard = (GridPane) root.lookup("#labelBoard");
+
+        leftNumbers = (VBox) root.lookup("#leftNumbers");
+        topNumbers = (HBox) root.lookup("#topNumbers");
+
+        createBoard(buttonBoard, labelBoard, leftNumbers, topNumbers);
 
         canvas = (Canvas) root.lookup("#canvas");
         graphicsContext = canvas.getGraphicsContext2D();
@@ -59,7 +71,7 @@ public class Controller {
         drawPiece(ChessBoard.pieceLocations[2][7]);
     }
 
-    public void createBoard(GridPane board, GridPane boardBackground) {
+    public void createBoard(GridPane buttonBoard, GridPane labelBoard, VBox leftNumbers, HBox topNumbers) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Label tileBackground = new Label("");
@@ -68,7 +80,7 @@ public class Controller {
                 Button tile = new Button("");
                 tile.setId(i + " " + (j));
                 tile.getStyleClass().add("boardTiles");
-                tile.setOnAction(this::tilePressed);
+                tile.setOnAction(event -> tilePressed(event, buttonBoard, leftNumbers, topNumbers));
                 tileArray[i][j] = tile;
 
                 if (i % 2 == 0) {
@@ -80,13 +92,27 @@ public class Controller {
                     else tileBackground.getStyleClass().add("evenTiles");
                 }
 
-                boardBackground.add(tileBackground, i, 7 - j);
-                board.add(tileArray[i][j], i, 7 - j);
+                labelBoard.add(tileBackground, i, 7 - j);
+                buttonBoard.add(tileArray[i][j], i, 7 - j);
             }
+        }
+
+        Label padding = new Label(String.valueOf(""));
+        padding.getStyleClass().add("tile");
+        topNumbers.getChildren().add(padding);
+
+        for (int i = 0; i < 8; i++) {
+            Label number = new Label(String.valueOf(8 - i));
+            Label letter = new Label(String.valueOf((char) ('A' + i)));
+            number.getStyleClass().add("tile");
+            letter.getStyleClass().add("tile");
+
+            leftNumbers.getChildren().add(number);
+            topNumbers.getChildren().add(letter);
         }
     }
 
-    public void tilePressed(ActionEvent event) {
+    public void tilePressed(ActionEvent event, GridPane buttonBoard, VBox leftNumbers, HBox topNumbers) {
         Button tile = (Button) event.getSource();
         int x = tile.getId().charAt(0) - '0';
         int y = tile.getId().charAt(2) - '0';
@@ -113,6 +139,7 @@ public class Controller {
                     tileArray[i][j].setDisable(false);
                 }
             }}
+
             selectX = x; selectY = y;
             lockIntoPiece = true;
         }
@@ -123,20 +150,32 @@ public class Controller {
                 playingSide = (playingSide.equals("White")) ? "Black" : "White";
             }
 
-            drawBoard(tileArray);
+            drawBoard(tileArray, buttonBoard, leftNumbers, topNumbers);
 
             selectX = -1; selectY = -1;
             lockIntoPiece = false;
         }
     }
 
-    public static void drawBoard(Button[][] tileArray) {
+    public static void drawBoard(Button[][] tileArray, GridPane buttonBoard, VBox leftNumbers, HBox topNumbers) {
         for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             tileArray[i][j].setDisable(false);
             tileArray[i][j].getStyleClass().remove("boardTilesPossibleMoves");
             if (ChessBoard.pieceLocations[i][j] != null) drawPiece(ChessBoard.pieceLocations[i][j]);
         }}
+
+        for (int i = 0; i < 8; i++) {
+            Label left = (Label)(leftNumbers.getChildren().get(i));
+            System.out.println(left.getText().charAt(0) - '0');
+            int pos = Math.abs(8 - (left.getText().charAt(0) - '1'));
+            left.setText("" + pos);
+
+            Label top = (Label)(topNumbers.getChildren().get(i + 1));
+            top.setText(String.valueOf((char)(pos + 'A' - 1)));
+        }
+
+        buttonBoard.setRotate((buttonBoard.getRotate() == 180) ? 0 : 180);
     }
 
     public static void drawPiece(ChessBoard piece) {
@@ -155,10 +194,18 @@ public class Controller {
 
         System.out.println(piece.pieceX + " " + piece.pieceY);
 
-        graphicsContext.drawImage(source,
-                pieceSource * 48, pieceColor * 48, 48, 48,
-                piece.pieceX * 60, piece.pieceY * (-60), 60, 60
-        );
+        if (playingSide.equals("White")) {
+            graphicsContext.drawImage(source,
+                    pieceSource * 48, pieceColor * 48, 48, 48,
+                    piece.pieceX * 60, piece.pieceY * (-60), 60, 60
+            );
+        }
+        else {
+            graphicsContext.drawImage(source,
+                    pieceSource * 48, pieceColor * 48, 48, 48,
+                    (7 - piece.pieceX) * 60, (7 - piece.pieceY) * (-60), 60, 60
+            );
+        }
     }
 
     public static void clearCanvas() {
