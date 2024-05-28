478f25748f576c5eda0614f4aa93a849e0b6fd85
Nguyen Pham <unknownuser2041@gmail.com>
2024-05-27 21:42:33 -0400

    commit

--- src/main/java/cockyadolescents/truechessthegame/WaitingRoom.java
+++ src/main/java/cockyadolescents/truechessthegame/WaitingRoom.java
@@ -7,17 +7,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import static cockyadolescents.truechessthegame.Main.*;
 
 // Online game
 public class WaitingRoom {
     Parent root;
     Scene scene;
 
+    @FXML
+    public void home() throws IOException {
+        homepage.display();
+    }
+
     public void queue() throws IOException {
         root = FXMLLoader.load(getClass().getResource("waitingroom.fxml"));
         scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        Main.window.setScene(scene);
+        window.setScene(scene);
         Game.onlineGame = true; // to change some features in the normal game
     }
 }
